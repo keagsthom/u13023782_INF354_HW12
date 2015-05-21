123b9490796f1cc8d48785ff3c06a1c1778c63c2
selloT <u13062060@tuks.co.za>
2015-05-21 12:57:09 +0200

    I added my name to the php

--- names.php
+++ names.php
@@ -2,5 +2,6 @@
 
 /* Add your names and student numbers here */
 echo '<h2>Keagan Thompson u13023782</h2>';
-echo '<h3> Poloko Mochotlhi u13035488 </h3> '; 
+echo '<h3> Poloko Mochotlhi u13035488 </h3> ';
+echo '<h1> Sello Thosago u13062060 </h1>';
 ?>
